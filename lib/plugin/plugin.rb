5fd39168cc0165fc06653bb2e2b00c04a25b21a5
Aaron Blythe <ablythe@hearst.com>
2016-10-06 14:39:43 -0500

    Plugin objects wire framing

--- lib/plugin/plugin.rb
+++ lib/plugin/plugin.rb
@@ -2,12 +2,30 @@
 require 'yaml'
 require 'docker'
 
-# Load plugins from Yaml File
+# Plugin class will represent an individual plugin.
+# It will check the metadata of the type of plugins to make decisions.
+# It's two main functions are to:
+#  1. Load the configuration of the specific plugin and load anything needed for that type.
+#  2. Execute the command properly based on the type
 class Plugin
+  module TypeEnum
+    SCRIPT = 1
+    CONTAINER = 2
+    API = 4
+  end
+
   def initialize(file)
     @name = File.basename(file)
     @config = YAML.load_file(file)
   end
 
+
+  def load
+
+  end
+
+  def exec
+    # based on some meta information like the type then execute the proper way
+  end
 
 end
--- lib/plugin/plugins.rb
+++ lib/plugin/plugins.rb
@@ -1,28 +1,55 @@
 # frozen_string_literal: true
-require 'yaml'
-require 'docker'
+#require 'yaml'
 require_relative 'plugin'
 
-# Load plugins from Yaml File
+# Plugins class will act as a cache of the plugins currently loaded.
+# It's two main functions are to:
+#  1. Load the configuration of all plugins
+#  2. Route the execution to the right plugin
 class Plugins
+  # TODO: determine if this hash is needed outside of this class
+  attr_reader :plugin_hash
+
   def initialize
-    @yaml_files = Dir.glob(File.expand_path('../../plugins/*.yml', File.dirname(__FILE__)))
     @plugin_hash = {}
-    @yaml_files.each do | file |
-      plugin = Plugin.new(file)
-      @plugin_hash[File.basename(file)] = plugin
+    load
+  end
+
+  # Loads the plugin configuration.
+  # Intention is that this is called on startup, however can also be called at any time
+  # during execution to reload
+  #
+  # Currently does not take any parameters nor does it return anything.
+  # Future iterations should allow for configuration based on commands from chat.
+  def load
+    #TODO: play with where we want the plugin configuration to live.
+    yaml_files = File.expand_path('../../plugins/*.yml', File.dirname(__FILE__))
+    Dir.glob(yaml_files).each do |file|
+      @plugin_hash[File.basename(file)] = Plugin.new(file)
     end
   end
 
-
-  def load
-    #@PluginLoad.each | plugin, value|
-    #  pluginObject = Plugin.new(plugin)
-    #end
+  # Routes the execution to the correct plugin if it exists.
+  #
+  # Splits off the first word it encounters and looks for a plugin with this name.
+  # If the plugin exists then it send the command on.
+  # If the plugin does not exist then it
+  # @param string command
+  # @return boolean - whether the command was passed on
+  def exec(command)
+    # if contains a space
+    if command.include? ''
+      requested_plugin = command.match(" ").pre_match
+      @plugin_hash.each do |name, plugin|
+        if name == requested_plugin
+          # TODO: may need to either:
+          #   return the return from plugin.exec
+          #   or make a call through the API to post in the channel
+          plugin.exec command.match(" ").post_match
+          return true
+        end
+      end
+    end
+    false
   end
-
-
-  #def exec
-  #  if fails
-  #end
 end
