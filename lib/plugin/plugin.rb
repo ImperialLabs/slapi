8adf54afaa0b8d3f25fabd73c08017d3263eea20
Levi Smith <levi@fynx.me>
2016-10-11 22:19:14 -0500

    cleanup of plugin.rb, formatting and linter stuff, merging uneeded excess, deleting obsulete or uneeded items that had been commented out from testing

--- lib/plugin/plugin.rb
+++ lib/plugin/plugin.rb
@@ -27,30 +27,30 @@ class Plugin
 
 
   # TODO: use name or label to match up with the config name
+  # Setup plugin by type
   def load
     case @config['plugin']['type']
     when 'script'
       filename = "#{@name}#{@lang_settings[:file_type]}"
       File.open("scripts/#{filename}", 'w') do |file|
-        file.write(@config['plugin']['write'])
+        file.write(@config[:plugin][:write])
       end
       File.chmod(0777, "scripts/#{filename}")
-      #File.open( filename, "#{@config['plugin']['write']}")
-      image = Docker::Image.create(fromImage: @lang_settings[:image])
-      #@container = Docker::Container.create(Image: @lang_settings[:image])
-      command = ["bash", "-c", "if [ -t 1 ]; then echo -n \"I'm a TTY!\"; fi"]
-      config_hash = {Image: @lang_settings[:image],
-                    HostConfig: {
-                      Binds: ["#{Dir.pwd}/scripts/#{filename}:/scripts/#{filename}"]
-                    },
-                    Cmd: command,
-                    Entrypoint: "/scripts/#{filename}",
-                    Tty: true}
-      puts "config_hash: \n\n  #{config_hash}"
-      
+      _image = Docker::Image.create(fromImage: @lang_settings[:image])
+      config_hash = {
+        name: @name,
+        Image: @lang_settings[:image],
+        HostConfig: {
+          Binds: ["#{Dir.pwd}/scripts/#{filename}:/scripts/#{filename}"]
+        },
+        Cmd: ["bash", "/scripts/#{filename}"],
+        Entrypoint: "/scripts/#{filename}",
+        Tty: true
+      }
       @container = Docker::Container.create(config_hash)
     when 'container'
       # load the docker container set in the config
+      _image = Docker::Image.create(fromImage: @lang_settings[:image])
       @container = Docker::Container.create(@config[:config])
     when 'api'
       # TODO httparty config
@@ -68,15 +68,9 @@ class Plugin
     # based on some meta information like the type then execute the proper way
     case @config['plugin']['type']
     when 'script', 'container'
-      #output = @container.run([data_from_chat])
-      #output = @container.run('ls -l')
-      
-      #command = ["bash", "-c", "if [ -t 1 ]; then echo -n \"I'm a TTY!\"; fi"]
-      #container = Docker::Container.create('Image' => 'ubuntu', 'Cmd' => command, 'Tty' => true)
       @container.tap(&:start).attach(:tty => true)
+      # TODO find a better way to get stdout or only last log entry? Possibly delete/recreate each time? If not, it will post the entire log into chat.
       output = @container.logs(stdout: true)
-      puts output
-
     when 'api'
       response = HTTParty.get(@config['api']['url'])
       puts response
@@ -87,7 +81,7 @@ class Plugin
   end
 
   # TODO likely move to a helper class
-  def lang_settings 
+  def lang_settings
     lang = {}
     case @config['plugin']['language']
     when 'ruby', 'rb'
@@ -111,6 +105,4 @@ class Plugin
     end
     lang
   end
-
-
 end
