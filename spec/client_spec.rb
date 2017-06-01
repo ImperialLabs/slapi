183af3a2285602ee7a58c1679ad9648d0a0a0c1a
Aaron Blythe <ablythe@hearst.com>
2017-06-01 14:01:35 -0500

    Update gems, modify tests to have ID in one place to change

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Bot, '#exec' do
 
   describe 'run bot tests' do
     before(:all) do
+      @bot_id = 'U4DEAQX1T'
       @bot = Bot.new(MockSettings.new)
       @bot.run
     end
@@ -16,43 +17,43 @@ RSpec.describe Bot, '#exec' do
     end
 
     it 'runs bot ping' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> ping')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> ping")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq('PONG')
     end
 
     it 'runs bot reload' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> reload')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> reload")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq('Plugins Reloaded Successfully')
     end
 
     it 'runs bot get help' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> help')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> help")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['fallback']).to eq('Your help has arrived!')
     end
 
     it 'runs bot get help for plugin' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> help parse')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> help parse")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['fallback']).to eq('Your help has arrived!')
     end
 
     it 'runs bot get help for bad plugin' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> help no_plugin')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> help no_plugin")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq('Sorry <@ABC123>, I did not find any help commands or plugins to list')
     end
 
     it 'runs bot plugin' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> hello world')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> hello world")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq("Hello World!\n")
     end
 
     it 'runs bot bad plugin' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> no_plugin')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> no_plugin")
       response = @bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq('Sorry <@ABC123>, I did not understand or find that command.')
     end
@@ -60,6 +61,7 @@ RSpec.describe Bot, '#exec' do
 
   describe 'run bot tests' do
     before(:all) do
+      @bot_id = 'U4DEAQX1T'
       @dm_bot = Bot.new(MockSettings.new(help: { 'dm_user' => true, 'level' => 1 }))
       @dm_bot.run
     end
@@ -77,13 +79,13 @@ RSpec.describe Bot, '#exec' do
     end
 
     it 'calls plugin from DM with bot name' do
-      mock = MockData.new(channel: 'D4FJHFBCK', text: '<@U4DEAQX1T> hello world', user: 'U4F0KFA73')
+      mock = MockData.new(channel: 'D4FJHFBCK', text: "<@#{@bot_id}> hello world", user: 'U4F0KFA73')
       response = @dm_bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq("Hello World!\n")
     end
 
     it 'calls plugin from DM with args without bot name' do
-      mock = MockData.new(channel: 'D4FJHFBCK', text: '<@U4DEAQX1T> hello', user: 'U4F0KFA73')
+      mock = MockData.new(channel: 'D4FJHFBCK', text: "<@#{@bot_id}> hello", user: 'U4F0KFA73')
       response = @dm_bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['text']).to eq("No World!\n")
     end
@@ -95,25 +97,25 @@ RSpec.describe Bot, '#exec' do
     end
 
     it 'runs bot get help list and expects DM' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> help', user: 'U4F0KFA73')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> help", user: 'U4F0KFA73')
       response = @dm_bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['fallback']).to eq('Your help has arrived!')
     end
 
     it 'runs bot get help and expects DM' do
-      mock = MockData.new(channel: 'C445NT42J', text: '<@U4DEAQX1T> help parse', user: 'U4F0KFA73')
+      mock = MockData.new(channel: 'C445NT42J', text: "<@#{@bot_id}> help parse", user: 'U4F0KFA73')
       response = @dm_bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['fallback']).to eq('Your help has arrived!')
     end
 
     it 'runs bot get help list in DM' do
-      mock = MockData.new(channel: 'D4FJHFBCK', text: '<@U4DEAQX1T> help', user: 'U4F0KFA73')
+      mock = MockData.new(channel: 'D4FJHFBCK', text: "<@#{@bot_id}> help", user: 'U4F0KFA73')
       response = @dm_bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['fallback']).to eq('Your help has arrived!')
     end
 
     it 'runs bot get help in DM' do
-      mock = MockData.new(channel: 'D4FJHFBCK', text: '<@U4DEAQX1T> help parse', user: 'U4F0KFA73')
+      mock = MockData.new(channel: 'D4FJHFBCK', text: "<@#{@bot_id}> help parse", user: 'U4F0KFA73')
       response = @dm_bot.listener(mock.constructed)
       expect(response['message']['attachments'][0]['fallback']).to eq('Your help has arrived!')
     end
